8645820731d4f3174b3464e2811f7978454b7e11
Julien Palmas <julien.palmas@gmail.com>
2011-10-20 12:28:21 +0200

    nest_attributes is now more generic thx to nested_attributes_options

--- lib/importer/active_record_ext/nest_attributes.rb
+++ lib/importer/active_record_ext/nest_attributes.rb
@@ -1,14 +1,15 @@
 module Importer
   module ActiveRecord
     module NestAttributes
-      
+
       def nest_attributes(flat_attributes)
-        
+
         flat_attributes.inject(nested_attributes = {}) do |memo, pair|
 
           key, v = pair.first, pair.last
 
-          unless key =~ /^(course|service|sub_service)_(\d+)_(.+)/
+          possible_keys = nested_attributes_options.keys.map { |key| key.to_s.singularize }.join('|')
+          unless key =~ /^(#{possible_keys})_(\d+)_(.+)/
             memo[key] = v
           else
             nested_key = "#{$1.pluralize}_attributes"
@@ -24,9 +25,9 @@ module Importer
         end
 
       end
-      
+
     end
-    
+
     ::ActiveRecord::Base.extend(NestAttributes)
   end
 end
