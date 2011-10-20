3c75b19731c680007601895aa82465b4066ab5e4
Julien Palmas <julien.palmas@gmail.com>
2011-10-20 14:43:35 +0200

    new nest_attributes on active_record instance

--- lib/importer/active_record_ext/nest_attributes.rb
+++ lib/importer/active_record_ext/nest_attributes.rb
@@ -1,121 +1,91 @@
 module Importer
   module ActiveRecord
     module NestAttributes
+      
+      module ClassMethods
 
-      def nest_attributes(flat_attributes)
+        def nest_attributes(flat_attributes)
 
-        flat_attributes.inject(nested_attributes = {}) do |memo, pair|
+          flat_attributes.inject(nested_attributes = {}) do |memo, pair|
 
-          key, v = pair.first, pair.last
+            key, v = pair.first, pair.last
 
-          possible_keys = nested_attributes_options.keys.map { |key| key.to_s.singularize }.join('|')
-          unless key =~ /^(#{possible_keys})_(\d+)_(.+)/
-            memo[key] = v
-          else
-            nested_key = "#{$1.pluralize}_attributes"
-            index = $2
-            sub_key = $3
+            possible_keys = nested_attributes_options.keys.map { |key| key.to_s.singularize }.join('|')
+            unless key =~ /^(#{possible_keys})_(\d+)_(.+)/
+              memo[key] = v
+            else
+              nested_key = "#{$1.pluralize}_attributes"
+              index = $2
+              sub_key = $3
 
-            memo[nested_key] ||= {}
-            memo[nested_key][index] ||= {}
+              memo[nested_key] ||= {}
+              memo[nested_key][index] ||= {}
 
-            sub_attributes = begin
-              $1.camelize.constantize.nest_attributes(sub_key => v)
-            rescue NameError
-              {sub_key => v}
+              sub_attributes = begin
+                $1.camelize.constantize.nest_attributes(sub_key => v)
+              rescue NameError
+                {sub_key => v}
+              end
+
+              memo[nested_key][index].deep_merge! sub_attributes
             end
 
-            memo[nested_key][index].deep_merge! sub_attributes
+            memo
           end
 
-          memo
         end
-
+        
       end
 
-    end
+      module InstanceMethods
+        
+        def nest_attributes(flat_attributes)
+          
+          attribs = self.class.nest_attributes flat_attributes
+          
+          h = {}
 
-    module PrepareNestesAttributes
+          attribs.each do |k, v|
 
-      def prepare_nested_attributes(attributes)
-        h = {}
+            possible_nested_keys = nested_attributes_options.keys.map { |key| "#{key}_attributes" }
 
-        attributes.each do |k, v|
+            if possible_nested_keys.include? k
+              
+              association = k.gsub("_attributes", "")
+              v.each do |s_k, s_v|
 
-          if k == "services_attributes"
+                associated_object = self.send(association).detect {|s| s.name == s_v["name"]}
 
-            v.each do |s_k, s_v|
-
-              s = services.detect {|s| s.name == s_v["name"]}
-
-              if s
-
-                h["services_attributes"] ||= {}
-                h["services_attributes"][s_k] ||= {}
-                h["services_attributes"][s_k] = s_v.merge({"id" => s.id.to_s})
-
-                s_v.each do |sss_k, sss_v|
-                  if sss_k == "sub_services_attributes"
-
-                    sss_v.each do |ss_k, ss_v|
-                      ss = s.sub_services.detect {|ss| ss.name == ss_v["name"]}
-
-                      if ss
-                        h["services_attributes"] ||= {}
-                        h["services_attributes"][s_k] ||= {}
-                        h["services_attributes"][s_k]["sub_services_attributes"] ||= {}
-                        h["services_attributes"][s_k]["sub_services_attributes"][ss_k] = ss_v.merge({"id" => ss.id.to_s})
-                      else
-                        h["services_attributes"][s_k] ||= {}
-                        h["services_attributes"][s_k].merge(s_v)
-                      end
-
-                    end
-
-                  else
-                    h["services_attributes"] ||= {}
-                    h["services_attributes"][s_k] ||= {}
-                    h["services_attributes"][s_k][sss_k] = sss_v
-                  end
+                if associated_object
+                  h[k] ||= {}
+                  h[k][s_k] ||= {}
+                  h[k][s_k] = associated_object.nest_attributes(s_v).merge({"id" => associated_object.id.to_s})
+                else
+                  h[k] ||= {}
+                  h[k].merge!(v)
                 end
 
-              else
-                h[k] ||= {}
-                h[k].merge!(v)
               end
-
-            end
-          elsif k == "courses_attributes"
-
-            v.each do |s_k, s_v|
-
-              course = courses.detect {|c| c.name == s_v["name"]}
-
-              if course
-                h["courses_attributes"] ||= {}
-                h["courses_attributes"][s_k] ||= {}
-                h["courses_attributes"][s_k] = s_v.merge({"id" => course.id.to_s})
-              else
-                h[k] ||= {}
-                h[k].merge!(v)
-              end
+            else
+              h[k] = v
             end
 
-          else
-            h[k] = v
           end
 
-        end
-
-        h
+          h
+        end        
       end
 
+      def self.included(receiver)
+        receiver.extend         ClassMethods
+        receiver.send :include, InstanceMethods
+      end
+      
     end
 
   end
 end
 
 class ActiveRecord::Base
-  extend Importer::ActiveRecord::NestAttributes
-  include Importer::ActiveRecord::PrepareNestesAttributes
+  include Importer::ActiveRecord::NestAttributes
 end
